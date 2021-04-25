44c48db8e470b5625f6e61a65426f0bfc1993510
tomra <tom.raeves3we@gmail.com>
2021-04-25 11:38:40 +0200

    streamline comments

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -8,13 +8,15 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
 
-        Socket soc = new Socket(("localhost"),PORT); //We setup a connection between the client and the host on IP-address "localhost" and PORT (5000)
+        Socket soc = new Socket(("localhost"),PORT);
+        //We setup a connection between the client and the host on IP-address "localhost" and PORT (5000)
         System.out.println("Connection succesfull!");
 
         byte [] b = new byte[2002];
         InputStream is = soc.getInputStream(); //We read the output stream of the server by making an input stream on the client
         is.read(b, 0, b.length); //the stream gets read
-        FileOutputStream fr = new FileOutputStream("C:\\Users\\tomra\\Documents\\receivedFromServer.txt"); //name the file and specify the place where we want to copy the file to
+        FileOutputStream fr = new FileOutputStream("C:\\Users\\tomra\\Documents\\receivedFromServer.txt");
+        //name the file and specify the place where we want to copy the file to
         fr.write(b, 0, b.length); //then we fill the new file with the content of the original file
 
         fr.close(); //When the client has received the file everything can be closed (streams and socket)
